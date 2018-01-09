b12d89484fba137b3d87142a5d19cc9d89cea6bc
acsdeveloper <developer@agilexcyber.in>
2018-01-09 19:05:44 +0530

    add validation for submit form in permission page

--- www/app/permission/permission.controller.js
+++ www/app/permission/permission.controller.js
@@ -6,9 +6,7 @@
 
         vm.isOffice = true;
         vm.userInfo = JSON.parse(localStorage.getItem("authDetails"));
-        console.log(vm.userInfo);
         vm.time = vm.userInfo.custom_fields;
-        vm.timeZone = "AM";
         vm.isTimeValid = true;
         vm.isMinutesValid = true;  
         
@@ -36,11 +34,14 @@
         vm.officeStartTime = $filter('date')(new Date(vm.officeTime.value), "h:mm a");
         vm.officeEntryTime = $filter('date')(new Date(), "h:mm a");
 
+
         vm.startTime = moment(vm.officeStartTime, "HH:mm a");
         vm.endTime = moment(vm.officeEntryTime, "HH:mm a");
         vm.hours = vm.endTime.diff(vm.startTime, 'hours');
         vm.minutes = vm.endTime.diff(vm.startTime, 'minutes');
-    
+        
+        vm.dayStartTime = new Date(vm.officeTime.value);
+
         if(vm.minutes >= 0 && vm.minutes <= 59 && vm.hours == 0) {
             vm.isLate = false;
         } else {
@@ -50,32 +51,63 @@
         // if(NetworkInformation.hasNetworkConnection()) {
             
         // }
-
+        vm.isLate = false;
         if(NetworkInformation.hasWifiConnection()) {
               
         }
 
     
-        vm.submitPermission = function () {   
-            vm.lateDetail = localStorage.getItem('lateDetails');
-            if(vm.lateDetail == null) {
-                console.log(vm.lateDetail);    
+        vm.submitPermission = function () {  
+            vm.data = {};
+            if(vm.isNull(vm.dayStartTime)) {
+                vm.isTimeValid = false;
+            } else if(vm.isNull(vm.requestTime)) {
+                vm.isMinutesValid = false;
             } else {
-
+                if(vm.count == 0) {
+                    vm.data = {
+                        "time_entry" : {
+                            "project_id": 227,
+                            "hours": "0.20",
+                            "activity_id": 15,
+                            "comments": "Permission for "+ vm.requestTime + " Minutes"
+                        }
+                    }
+                    vm.addPermission(vm.data);
+                } else if (vm.count > 0) {
+                    vm.data = {
+                        "time_entry" : {
+                            "project_id": 227,
+                            "hours": "0.40",
+                            "activity_id": 15,
+                            "comments": "Update Permission for "+ vm.requestTime + " Minutes"
+                        }
+                    }
+                    vm.updatePermission(vm.entry_id, vm.data);
+                }
+                
             }
-            vm.isOffice = false;
-            var obj = {
-                'today': "entry"
-            }
-            localStorage.setItem('lateDetails', obj);
         }
 
-        vm.officeClick = function () {
+        vm.addPermission = function (data) {
             vm.auth = localStorage.getItem("authoptions");
             $http.defaults.headers.common['Authorization'] = vm.auth;
             vm.authdata.headers.Authorization = vm.auth;
             AuthInterceptor.request(vm.authdata);
-            PermissionService.getPermission(vm.data).then(function (resp) {
+            PermissionService.addPermission(data).then(function (resp) {
+                console.log(resp.time_entry.id);
+                //vm.new_entry = resp.time_entry;
+                localStorage.setItem("permission_09-01-2018", JSON.stringify(resp.time_entry.id));
+                vm.permission();
+            })
+        }
+
+        vm.updatePermission = function (Id,data) {
+            vm.auth = localStorage.getItem("authoptions");
+            $http.defaults.headers.common['Authorization'] = vm.auth;
+            vm.authdata.headers.Authorization = vm.auth;
+            AuthInterceptor.request(vm.authdata);
+            PermissionService.updatePermission(Id,data).then(function (resp) {
                 vm.permission();
             })
         }
@@ -91,6 +123,14 @@
             AuthInterceptor.request(vm.authdata);
             PermissionService.permissionList(vm.userInfo.id).then(function(resp)  {
                 vm.count = resp.total_count;
+                console.log(resp);
+                // vm.list = resp.time_entries;
+                if(localStorage.getItem("permission_09-01-2018")) {
+                    vm.entry_id = localStorage.getItem("permission_09-01-2018");
+                }
+                // vm.current = vm.list.filter(function(data) {
+                //     return data.id = vm.entry_id
+                // })
                 if(vm.count > 0) {
                     vm.isOffice = true;
                 } else {
@@ -100,21 +140,24 @@
         }
 
         vm.enterMinutes = function() {
-            var regx = /^([1-5]?[1-9])$/.test(vm.requestTime);
-            if(regx) {
+            if(vm.requestTime > 0 && vm.requestTime <= 60) {
                 vm.isMinutesValid = true;
             } else {
-                vm.isMinutesValid = false;  
-            }
+               vm.isMinutesValid = false;  
+            } 
+            
         }
 
         vm.enterTime = function() {
-            var regx = /^([0-1][0-9]):([0-5][0-9])$/.test(vm.dayStartTime);
-            if(regx) {
-                vm.isTimeValid = true;
-            } else {
+            if(vm.isNull(vm.dayStartTime)) {
                 vm.isTimeValid = false;
+            } else {
+                vm.isTimeValid = true;
             }
+        }
+
+        vm.isNull = function(value) {
+            return typeof value == 'undefined' || value == null || value == 'null' || value == '';
         }
 
         vm.permission(); 
